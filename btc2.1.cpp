2449fed3d23f5f0d89ab7b023a3e7d8c78551051
tranduc2601 <breakgamer2601@gmail.com>
2024-12-10 11:55:47 +0700

    Add files via upload

---
+++ btc2.1.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+	int a; //khai bao bien so nguyen
+	float b; // khai bao bien so thuc
+	double c; //khai bao bien so thuc kich thuoc rong hon 
+	char d; //khai bao bien ki tu 
+	return 0; 
+} 
