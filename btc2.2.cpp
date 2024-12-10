af4fe2d6288214ec3f434a216ef4329a79453d06
tranduc2601 <breakgamer2601@gmail.com>
2024-12-10 19:22:34 +0700

    Add files via upload

---
+++ btc2.2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	int a,b;
+	printf("nhap so nguyen a: ");
+	scanf("%d", &a); 
+	printf(" nhap so nguyen b: ");
+	scanf("%d", &b);
+	int sum= a+b; 
+	printf("tong cua %d va %d la: %d \n", a,b,sum); 
+	return 0; 
+} 
